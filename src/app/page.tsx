c6d0948314b30c832255010fc2cef27b6b470ad3
raghavt3 <raghavt3@illinois.edu>
2024-04-11 15:40:41 -0500

    initial design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,112 +2,88 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="justify-center min-h-screen bg-[#fb921a]">
+      <div className="w-full">
+        {/* horizontal navbar */}
+        <nav className="bg-[#ffb703] relative">
+          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-10 pt-5">
+            <div className="flex items-center space-x-3">
+              <Image src="/logo.PNG" alt="Carbon" width={80} height={80}/>
+              <span className="text-3xl font-semibold whitespace-nowrap dark:text-black">Hindu YUVA UIUC</span>
+            </div>
+            <div className="block pb-4">
+              <ul className="flex flex-col font-medium md:p-0 mt-4 borderrounded-lg md:space-x-4 rtl:space-x-reverse md:flex-row">
+              <li>
+                  <a className="block py-2 pr-3 text-black hover:text-red-900 rounded">Team</a>
+                </li>
+                <li>
+                  <a className="block py-2 pr-3 text-black hover:text-red-900 rounded">Events</a>
+                </li>
+                <li>
+                  <a className="block py-1 pl-3 pr-3 my-1 text-white bg-[#ae4225] rounded">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="custom-shape-divider-top-1712860733">
+          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
+          </svg>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="w-full p-10">
+          <h1 className="text-5xl text-white pb-10 pt-10 text-center font-bold">"Arise, awake, and stop not until the goal is achieved."</h1>
+          <h1 className="text-3xl text-white text-center font-serif italic">- Swami Vivekananda</h1>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="custom-shape-divider-bottom-1712864080">
+          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+              <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
+          </svg>     
       </div>
+      <div className="bg-[#e75480] h-20">
+
+      </div>
+      <div className="bg-[#e75480] h-screen pt-18">
+            <div className="grid grid-cols-1 lg:grid-cols-2">
+                <div className=" text-white bg-[#e75480] lg:h-screen h-fit">
+                    <div className="flex items-center justify-left w-full h-full p-10">
+                        <div className="w-full">     
+                            <h1 className="text-5xl font-extrabold text-white w-full pb-5">CULTURE</h1>
+                            <p className="text-lg text-white w-full pb-8">
+                                Celebrating the vibrant culture of India, Hindu YUVA UIUC is a student organization that aims to promote the values of Hinduism and Indian culture. We host a variety of events throughout the year, including Garba, Diwali, and Holi, to bring the community together and celebrate our rich heritage.
+                            </p>
+                        </div>
+                    </div>                 
+                </div>
+                <div className="text-white bg-[#e75480] lg:h-screen h-fit w-full">
+                    <div className="flex items-center justify-center w-full h-full px-10 pt-5">
+                        {/* image 6704 */}
+                        <Image src="/temple.png" alt="6704" width={650} height={650} className="rounded-[15px]"/>       
+                    </div>
+                </div>
+                <div className=" text-white bg-[#e75480] lg:h-screen h-fit">
+                    <div className="flex items-center justify-left w-full h-full p-10">
+                        <div className="w-full">     
+                            <h1 className="text-5xl font-extrabold text-white w-full pb-5">COMMUNITY</h1>
+                            <p className="text-lg text-white w-full pb-8">
+                                Hindu YUVA UIUC is more than just a cultural organization – it’s a community. Our flagship space the Vivekananda House features a library, vegetarian kitchen, and multipurpose hall open to all. At Vivekananda House we also host weekly discussions, yoga classes, and other events to foster a sense of belonging and connection among our community.
+                            </p>
+                        </div>
+                    </div>                 
+                </div>
+                <div className="text-white bg-[#e75480] lg:h-screen h-fit w-full">
+                    <div className="flex items-center justify-center w-full h-full px-10 pt-5">
+                        {/* image 6704 */}
+                        <Image src="/fire.png" alt="6704" width={550} height={550} className="rounded-[15px]"/>       
+                    </div>
+                </div>
+            </div>
+            <div className="bg-[#e75480] h-20 w-full items-center pt-5">
+              <p className="text-center text-[9px]">Designed and Maintained by Raghav Tirumale</p>
+            </div>      
+      </div>
+      
     </main>
   );
 }
